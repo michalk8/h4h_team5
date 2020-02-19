0d5eef9c7968104136f6a461f0444b1fe66d7a56
Carlo De Donno <dedonno1@jrl11.jureca>
2020-02-19 16:28:59 +0100

    fix import metrics

--- model/my_metrics.py
+++ model/my_metrics.py
@@ -1,3 +1,5 @@
+from keras import backend as K
+
 def recall_m(y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
