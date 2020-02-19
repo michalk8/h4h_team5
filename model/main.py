7f08817969695902c40f287b799f9db38cc70080
Michal Klein <michal.klein@protonmail.com>
2020-02-19 17:16:27 +0100

    Remove dependency

---
+++ model/main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import pickle
+import argparse
+import tensorflow.keras.layers as layers
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from resnet import residual_network
+from my_metrics import *
+from util import *
+
+_resise_methods = dict(
+    area          = tf.image.ResizeMethod.AREA,
+    bilinear      = tf.image.ResizeMethod.BILINEAR,
+    bicubic       = tf.image.ResizeMethod.BICUBIC,
+    # gaussian      = tf.image.ResizeMethod.GAUSSIAN,
+    # lanczos3      = tf.image.ResizeMethod.LANCZOS3,
+    # lanczos5      = tf.image.ResizeMethod.LANCZOS5,
+    # mitchellcubic = tf.image.ResizeMethod.MITCHELLCUBIC,
+    nearest       = tf.image.ResizeMethod.NEAREST_NEIGHBOR
+)
+
+
+EPOCHS = 3
+BATCH_SIZE = 16
+IMG_W, IMG_H, IMG_C = 400, 400, 3
+
+
+def main(args):
+    train_dir = os.path.join(args.dataset_root, 'train')
+    test_dir = os.path.join(args.dataset_root, 'test')
+    total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+    total_test = sum([len(files) for r, d, files in os.walk(test_dir)])
+
+    image_tensor = layers.Input(shape=(IMG_H, IMG_W, IMG_C))
+    network_output = residual_network(image_tensor, args.img_size)
+
+    model = get_model(args.img_size, _resise_methods[args.downsampling])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', f1_m, precision_m, recall_m])
+    print(model.summary())
+
+    image_gen_train = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=359,
+        horizontal_flip=True,
+        vertical_flip=True,
+    )
+    validation_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our validation
+
+    train_data_gen = image_gen_train.flow_from_directory(batch_size=BATCH_SIZE,
+                                                         directory=train_dir,
+                                                         shuffle=True,
+                                                         target_size=(IMG_H, IMG_W),
+                                                         class_mode='categorical')
+    val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                                  directory=test_dir,
+                                                                  target_size=(IMG_H, IMG_W),
+                                                                  class_mode='categorical')
+
+    files_per_class = []
+    input_foldr = train_dir
+    for folder in os.listdir(input_foldr):
+        if not os.path.isfile(folder):
+            files_per_class.append(len(os.listdir(input_foldr + '/' + folder)))
+
+    total_files = sum(files_per_class)
+    class_weights = {}
+    for i in range(len(files_per_class)):
+        class_weights[i] = 1 - (float(files_per_class[i]) / total_files)
+
+    history = model.fit_generator(
+        train_data_gen,
+        steps_per_epoch=total_train // BATCH_SIZE,
+        epochs=EPOCHS,
+        validation_data=val_data_gen,
+        validation_steps=total_test // BATCH_SIZE,
+        class_weight=class_weights
+    )
+
+    with open('trainHistoryDict_{}_{}.pickle'.format(args.img_size, args.downsampling), 'wb') as fout:
+        pickle.dump(history.history, fout)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_root', type=str)
+    parser.add_argument('img_size', type=int)
+    parser.add_argument('downsampling', type=str)
+
+    main(parser.parse_args())
--- model/util.py
+++ model/util.py
@@ -1,4 +1,3 @@
-from keras_augm_layer import *
 from resnet import residual_network
 import tensorflow.keras.layers as layers
 from tensorflow.keras.models import Model
@@ -6,32 +5,17 @@ from tensorflow.keras.models import Model
 IMG_W, IMG_H, IMG_C = 400, 400, 3
 
 
-# TODO: albumentations
 def get_transforms():
-    return [
-        HorizontalFlip(p=0.5),
-        VerticalFlip(p=0.5),
-        RandomRotate(angle=45, p=0.5),
-        RandomRotate90(p=0.5),
-
-        RGBShift(p=0.1, r_shift_limit=20, g_shift_limit=20, b_shift_limit=20),
-        RandomBrightness(p=0.999, max_delta=0.1),
-        RandomContrast(0.5, 1.5, p=0.9),
-        RandomHue(0.5, p=0.9),
-        RandomSaturation(0.5, 1.5, p=0.9),
-
-        RandomGaussNoise((10, 50), p=0.99),
-        # ToGray(p=0.5),
-        JpegCompression(p=0.9, quality_lower=5, quality_upper=99),
-    ]
+    return []
 
 
-def get_model(img_size, downsampling, apply_transforms=False):
+def get_model(img_size, downsampling, apply_transforms=True):
     inp = layers.Input(shape=(IMG_H, IMG_W, IMG_C))
 
     if apply_transforms:
         transforms = get_transforms()
-        x = AugmLayer(transforms, output_dim=(IMG_H, IMG_W, IMG_C))(inp)
+        raise NotImplemented('TODO: implement')
+        x = inp
     else:
         x = inp
 
